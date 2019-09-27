d4531420313269eb424ca39fe565288bca561660
Alexey Igrychev <alexey.igrychev@flant.com>
2019-09-27 14:46:59 +0100

    [tests] Travis and codeclimate integration

---
+++ scripts/ci/codeclimate_after_script_hook.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+coverage_files=$WERF_TEST_COVERAGE_DIR/*.out
+for file in ${coverage_files[@]}
+do
+  file_name=$(basename $file)
+  ./cc-test-reporter format-coverage \
+    -t=gocov \
+    -p=github.com/flant/werf/ \
+    -o="coverage/$file_name.codeclimate.json" \
+    "$file"
+done
+
+./cc-test-reporter sum-coverage \
+  -p=$(ls -1q coverage/*.codeclimate.json | wc -l) \
+  coverage/*.codeclimate.json
+
+./cc-test-reporter upload-coverage
